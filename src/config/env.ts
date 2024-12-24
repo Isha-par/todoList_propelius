387dd8a6b8016da7b9fcf0524ca03a1fde497fc7
isha2012 <ishapatel2021@gmail.com>
2024-12-24 13:31:39 +0530

    Read me file updated and env sample added

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,9 +5,8 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 5000,
   mongoUri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/todoList-propelius",
-  jwtSecret: process.env.ACCESS_TOKEN_SECRET || "default_todo-Secret",
+  jwtSecret: process.env.ACCESS_TOKEN_SECRET,
   secretToken: process.env.ACCESS_TOKEN_SECRET,
   expiryToken : process.env.ACCESS_TOKEN_EXPIRY,
   emailPass : process.env.MAIL_PASS,
-  
 };
